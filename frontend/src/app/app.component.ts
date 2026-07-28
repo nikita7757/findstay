1530a0006b8cea72b7a7e98d96c8de8711a930b0
nikita <nikitashedage2004@gmail.com>
2026-07-28 16:39:38 +0530

    Remove server connection warming toast banner

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,54 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { BackendStatusService } from './services/backend-status.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'findstay_app';
-  isWakingUp = false;
-  isConnected = false;
-  hasError = false;
-  showBanner = false;
-  showSuccess = false;
-
-  constructor(private backendStatus: BackendStatusService) {}
-
-  ngOnInit(): void {
-    // Monitor isWakingUp state
-    this.backendStatus.isWakingUp$.subscribe(wakingUp => {
-      this.isWakingUp = wakingUp;
-      if (wakingUp) {
-        this.showBanner = true;
-      }
-    });
-
-    // Monitor isConnected state
-    this.backendStatus.isConnected$.subscribe(connected => {
-      this.isConnected = connected;
-      if (connected && this.showBanner) {
-        // Show success briefly, then hide banner
-        this.showSuccess = true;
-        setTimeout(() => {
-          this.showSuccess = false;
-          this.showBanner = false;
-        }, 2500);
-      }
-    });
-
-    // Monitor error state
-    this.backendStatus.hasError$.subscribe(error => {
-      this.hasError = error;
-      if (error) {
-        this.showBanner = true;
-      }
-    });
-  }
-
-  dismissBanner(): void {
-    this.showBanner = false;
-  }
 }
 
